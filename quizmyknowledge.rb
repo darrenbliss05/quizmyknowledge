dd40a964cdb47688980a622c76accd5467b8aae6
bliss <darrenbliss05@gmail.com>
2017-01-06 01:46:18 -0500

    refactor

--- quizmyknowledge.rb
+++ quizmyknowledge.rb
@@ -1,6 +1,37 @@
 require 'json'
-Shoes.app(title: "Quizme") do 
+Shoes.app(title: "Quizmyknowledge") do 
 
+def populatequestion()
+        foo = @bar.pop()
+        @question.text =  strong("Question. "), foo['question']
+        $option_list = ""
+        keylist = []
+        #print foo['options'].methods()
+        # Shuffle the answer keys so they are not the sam eeach time
+        tmpfoo = foo['options'].clone()
+        $answer = foo['answer']
+        for key in tmpfoo.keys do
+            keylist.push(key)
+        end
+        shuffledkeys = keylist.shuffle! 
+        shuffledoptions = {} 
+        tmpanswer = $answer
+        for opt in foo['options'].each do
+           keychange = shuffledkeys.pop()
+           shuffledoptions[keychange] = opt[opt.length() -1]
+           if opt[0] ==  tmpanswer then
+                #print "Changing answer from #{tmpanswer} to #{keychange}\n"
+                $answer = keychange
+                #print "new answer is ", $answer, "\n";
+            end
+           
+        end
+        for opt in shuffledoptions.keys.sort do
+               $option_list << "#{opt}" + " " + shuffledoptions[opt]  + "\n"
+        end
+        @options.text = $option_list
+        @response_answer.text = ""
+end
 
 filename = ask_open_file()
 fileinput = File.read(filename)
@@ -26,31 +57,24 @@ foobar = JSON.parse(fileinput)
    end
    resultline = para "     " 
    flow do
-     button ("next question") {
-       foo = @bar.pop()
-        @question.text =  strong("Question. "), foo['question']
-        $option_list = ""
-        for opt in foo['options'].each do
-             $option_list << "#{opt[0]}" + " " + opt[opt.length() -1]  + "\n"
-        end
-        @options.text = $option_list
-        resultline.text = "        "
-        $answer = foo['answer']
-        @response_answer.text = ""
-     }
      button ("Exit Quizme") {
          exit
      }
+     button ("next question") {
+       resultline.text = ""
+       populatequestion()
+     }
     end
   end
-key = @bar.pop()
-   @question.text =  strong("Question."), key['question'] 
-   $option_list = ""
-   #for opt in key['options'].each do
-   for opt in key['options'] do
-       $option_list.append("#{opt[0]} ", opt[opt.length() -1 ], "\n")
-   end
-   @options.text = $option_list  
-   $answer = key['answer']
+  populatequestion()
+#key = @bar.pop()
+#   @question.text =  strong("Question."), key['question'] 
+#   $option_list = ""
+#   #for opt in key['options'].each do
+#   for opt in key['options'] do
+#       $option_list.append("#{opt[0]} ", opt[opt.length() -1 ], "\n")
+#   end
+#   @options.text = $option_list  
+#   $answer = key['answer']
 end
 
